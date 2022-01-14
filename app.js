32c996f9a585364df9a1353a62974454b1275e1c
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-01-14 12:06:47 +0200

    Weather app

---
+++ app.js
@@ -0,0 +1,33 @@
+// LS object
+const ls = new LS()
+// init city
+const initCity = ls.getCityData()
+// Weather object
+const weather = new Weather(initCity)
+console.log(weather.getWeather())
+// UI object
+const ui = new UI()
+
+
+// events
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeCityWeather)
+
+function changeCityWeather(event){
+    const city = document.querySelector('#city-name').value
+    weather.changeCity(city)
+    ls.setCityData(city)
+    getWeather()
+    document.querySelector('#city-name').value = ''
+    event.preventDefault()
+}
+
+function getWeather(){
+    weather.getWeather()
+        .then(data => {
+            console.log(data)
+            ui.drawWeather(data)
+        })
+        .catch(error => console.log(error))
+}
+getWeather()
